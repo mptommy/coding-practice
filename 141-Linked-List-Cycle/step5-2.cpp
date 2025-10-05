6f418e406ec3c856e7bbfc6f95d2aba9dc04b642
mptommy <hirogonbe0121@gmail.com>
2025-10-05 09:59:40 +0900

    solve again

---
+++ 141-Linked-List-Cycle/step5-2.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <set>
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+		std::set<ListNode*> seen;
+		ListNode *cur_node = head;
+		while (cur_node) {
+			if (seen.contains(cur_node)) {
+				return true;
+			}
+			seen.insert(cur_node);
+			cur_node = cur_node->next;
+		}
+		return false;
+    }
+};
